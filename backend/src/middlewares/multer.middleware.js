e562250b05fdce8c6e5af04bfb2bc8700def491e
sourya-6 <saisourya678@gmail.com>
2025-03-19 16:56:59 +0530

    middleware changed for multiple photo uploads

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -7,6 +7,7 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
       
       cb(null, file.originalname)
+      //Date.now()+path.extname(file.originalname)-->creates a unique id for each file
     }
   })
   
