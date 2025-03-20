5c1fec5fb7b790b5558de83687cecd42d8dda378
sourya-6 <saisourya678@gmail.com>
2025-03-20 17:42:38 +0530

    s

--- backend/src/routes/codeExecution.routes.js
+++ backend/src/routes/codeExecution.routes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { executeCodeController, getCodeResult } from "../controllers/codeExecution.controller.js";
+import {
+  executeCodeController,
+  getCodeResult,
+} from "../controllers/codeExecution.controller.js";
 
 const router = express.Router();
 
