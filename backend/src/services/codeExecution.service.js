925ef5098b38cc43a0d9adfc02c022e38b33cb32
sourya-6 <saisourya678@gmail.com>
2025-07-21 19:46:45 +0530

    done some imp changes like using upstash

--- backend/src/services/codeExecution.service.js
+++ backend/src/services/codeExecution.service.js
@@ -2,10 +2,15 @@ import { createClient } from "redis";
 import { v4 as uuid } from "uuid";
 
 // const redisClient = createClient({ url: "redis://redis:6379" });
-const redisClient = createClient({
-  url: "redis://default:AWXQAAIjcDEzZTljMDZmOGIyZmQ0MDBlODY4MTNlMTAyZTBmMmVkZnAxMA@loyal-beetle-26064.upstash.io:6379"
-});
-await redisClient.connect();
+// const redisClient = createClient({
+//   url: "redis://default:AWXQAAIjcDEzZTljMDZmOGIyZmQ0MDBlODY4MTNlMTAyZTBmMmVkZnAxMA@loyal-beetle-26064.upstash.io:6379"
+// });
+import { Redis } from '@upstash/redis'
+const redisClient = new Redis({
+  url: 'https://loyal-beetle-26064.upstash.io',
+  token: 'AWXQAAIjcDEzZTljMDZmOGIyZmQ0MDBlODY4MTNlMTAyZTBmMmVkZnAxMA',
+})
+// await redisClient.connect();
 
 export const submitCode = async (userId, code, language, functionName, testCases) => {
   const jobId = uuid();
--- backend/src/utils/redisClient.js
+++ backend/src/utils/redisClient.js
@@ -1,17 +1,20 @@
 import { createClient } from "redis";
-
+import { Redis } from '@upstash/redis'
 // const redisClient = createClient({
 //   url: process.env.REDIS_URL || "redis://redis:6379",
 // });
-const redisClient = createClient({
-  url: "redis://default:AWXQAAIjcDEzZTljMDZmOGIyZmQ0MDBlODY4MTNlMTAyZTBmMmVkZnAxMA@loyal-beetle-26064.upstash.io:6379"
-});
-
+// const redisClient = createClient({
+//   url: "redis://default:AWXQAAIjcDEzZTljMDZmOGIyZmQ0MDBlODY4MTNlMTAyZTBmMmVkZnAxMA@loyal-beetle-26064.upstash.io:6379"
+// });
+const redisClient = new Redis({
+  url: 'https://loyal-beetle-26064.upstash.io',
+  token: 'AWXQAAIjcDEzZTljMDZmOGIyZmQ0MDBlODY4MTNlMTAyZTBmMmVkZnAxMA',
+})
 redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
 
 (async () => {
   try {
-    await redisClient.connect();
+    // await redisClient.connect();
     console.log("✅ Redis Connected");
   } catch (error) {
     console.error("❌ Redis Connection Failed", error);
