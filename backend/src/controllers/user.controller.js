683060d8c1fb5a747e939a5999104fe54f80c3d5
sourya-6 <saisourya678@gmail.com>
2025-07-21 19:01:41 +0530

    done some import changes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import {User} from "../models/user.models.js";
-import {ApiError} from "../utils/ApiError.js";
+import {ApiError} from "../utils/apiError.js";
 import {ApiResponse} from "../utils/apiResponse.js";
 import {asyncHandler} from "../utils/asyncHandler.js";
 
