4ddda1b1ada625b6e40a83e875e739393c5b6569
sourya-6 <saisourya678@gmail.com>
2025-07-21 19:26:57 +0530

    redis client token changed

--- backend/src/utils/redisClient.js
+++ backend/src/utils/redisClient.js
@@ -1,7 +1,11 @@
 import { createClient } from "redis";
 
+// const redisClient = createClient({
+//   url: process.env.REDIS_URL || "redis://redis:6379",
+// });
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://redis:6379",
+  url: process.env.REDIS_URL || 'https://loyal-beetle-26064.upstash.io',
+  token: process.env.REDIS_TOKEN || 'AWXQAAIjcDEzZTljMDZmOGIyZmQ0MDBlODY4MTNlMTAyZTBmMmVkZnAxMA',
 });
 
 redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
